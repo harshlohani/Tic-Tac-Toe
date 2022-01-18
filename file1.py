b905d73e13ab5c6f8a4ce035583d6d3464fc14d6
Harsh Lohani <lohani01harsh@gmail.com>
2022-01-19 01:40:11 +0530

    added module for inputting name of players

---
+++ file1.py
@@ -0,0 +1,24 @@
+
+
+print("Game started !!!")
+
+val1= input("'X' Enter your name: ")
+x_name=val1
+print(val1)
+
+val2= input("'O' Enter your name: ")
+o_name=val2
+print(val2)
+
+
+
+
+# to pass the turn to player alternatively
+    #def call2():
+     #   for i in range(9):
+      #   if(i%2)!=0:
+       #      print(f'{val1}'+"'s"+ " turn")
+        # elif(i%2)==0:
+         #    print(f'{val2}'+"'s"+" turn")
+
+#call2()
